72cf51c3a52ccbc5686d40ff2037dc49d375cff8
Yahweasel <git-rpo@yahweasel.com>
2020-11-26 18:13:11 -0500

    Modularizing WebRtcVad

--- vad/post.js
+++ vad/post.js
@@ -1,11 +1,8 @@
-WebRtcVad = {
-    Module: Module,
-    Create: Module.cwrap("WebRtcVad_Create", "number", []),
-    Init: Module.cwrap("WebRtcVad_Init", "number", ["number"]),
-    Free: Module.cwrap("WebRtcVad_Free", null, ["number"]),
-    set_mode: Module.cwrap("WebRtcVad_set_mode", "number", ["number"]),
-    Process: Module.cwrap("WebRtcVad_Process", "number", ["number", "number", "number", "number"]),
-    malloc: Module.cwrap("malloc", "number", ["number"]),
-    free: Module.cwrap("free", null, ["number"]),
-    heap: Module.HEAPU8
-};
+Module.Create = Module.cwrap("WebRtcVad_Create", "number", []);
+Module.Init = Module.cwrap("WebRtcVad_Init", "number", ["number"]);
+Module.Free = Module.cwrap("WebRtcVad_Free", null, ["number"]);
+Module.set_mode = Module.cwrap("WebRtcVad_set_mode", "number", ["number"]);
+Module.Process = Module.cwrap("WebRtcVad_Process", "number", ["number", "number", "number", "number"]);
+Module.malloc = Module.cwrap("malloc", "number", ["number"]);
+Module.free = Module.cwrap("free", null, ["number"]);
+Module.heap = Module.HEAPU8;
