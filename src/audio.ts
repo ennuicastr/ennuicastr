08e1d5900a11cfc2d4324c975a571fa40e02add5
Yahweasel <git-rpo@yahweasel.com>
2021-03-24 02:31:49 +0000

    Version bump (worker branch now master)

--- src/audio.ts
+++ src/audio.ts
@@ -110,7 +110,7 @@ export var useLibAV = false;
 export var useAWP = false;
 
 // Worker paths to use
-const workerVer = "0." + Math.random() + Math.random();
+const workerVer = "4";
 const awpPath = "awp/ennuicastr-awp.js?v=" + workerVer;
 export const workerPath = "awp/ennuicastr-worker.js?v=" + workerVer;
 
