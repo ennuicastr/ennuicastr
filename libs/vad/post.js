6d0555dd82ab471f02a3a4ecb2831bb6c94bd918
Yahweasel <git-rpo@yahweasel.com>
2021-12-21 12:56:59 -0500

    Correcting VAD set_mode

--- libs/vad/post.js
+++ libs/vad/post.js
@@ -1,7 +1,7 @@
 Module.Create = Module.cwrap("WebRtcVad_Create", "number", []);
 Module.Init = Module.cwrap("WebRtcVad_Init", "number", ["number"]);
 Module.Free = Module.cwrap("WebRtcVad_Free", null, ["number"]);
-Module.set_mode = Module.cwrap("WebRtcVad_set_mode", "number", ["number"]);
+Module.set_mode = Module.cwrap("WebRtcVad_set_mode", "number", ["number", "number"]);
 Module.Process = Module.cwrap("WebRtcVad_Process", "number", ["number", "number", "number", "number"]);
 Module.malloc = Module.cwrap("malloc", "number", ["number"]);
 Module.free = Module.cwrap("free", null, ["number"]);
