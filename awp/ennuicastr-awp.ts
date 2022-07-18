fb1d9f1dcaf516f7aee83a6d8055f7f379deef4f
Yahweasel <git-rpo@yahweasel.com>
2022-07-18 13:50:11 +0000

    First steps of the AWP handling multiple workers (software multiplexing)

--- awp/ennuicastr-awp.ts
+++ awp/ennuicastr-awp.ts
@@ -42,19 +42,25 @@ declare function registerProcessor(
 // Size of our shared buffer
 const bufSz = 96000;
 
+// Shared audio buffer with a read-write head
+interface SharedBuffer {
+    // A number of buffers equal to the number of channels
+    buffers: Float32Array[];
+
+    // And our read and write heads
+    heads: Int32Array;
+}
+
 // General-purpose processor for doing work in a Worker
 class WorkerProcessor extends AudioWorkletProcessor {
-    workerPort: MessagePort;
     canShared: boolean;
 
-    /* OUTGOING: a number of shared buffers equal to the number of channels,
-     * and a shared read/write head */
-    outgoing: Float32Array[];
-    outgoingRW: Int32Array;
+    // The workers we're communicating with
+    workerPorts: MessagePort[];
 
-    // INCOMING
-    incoming: Float32Array[];
-    incomingRW: Int32Array;
+    // The shared memory buffers with them
+    outgoing: SharedBuffer[];
+    incoming: SharedBuffer[];
 
     constructor(options?: AudioWorkletNodeOptions) {
         super(options);
@@ -63,40 +69,61 @@ class WorkerProcessor extends AudioWorkletProcessor {
         this.canShared =
             typeof SharedArrayBuffer !== "undefined";
 
+        this.workerPorts = [];
+        this.outgoing = [];
+        this.incoming = [];
+
         // The only message from the AWP port is the worker port
         this.port.onmessage = ev => {
             const msg = ev.data;
             switch (msg.c) {
                 case "workerPort":
-                    this.workerPort = msg.p;
-                    this.workerPort.onmessage = ev => {
+                {
+                    // Find an index
+                    let wi = 0;
+                    for (; wi < this.workerPorts.length && this.workerPorts[wi]; wi++)
+                    if (wi === this.workerPorts.length) {
+                        this.workerPorts.push(null);
+                        this.incoming.push(null);
+                        this.outgoing.push(null);
+                    }
+                    this.workerPorts[wi] = msg.p;
+
+                    // Prepare for data
+                    msg.p.onmessage = ev => {
+                        if (!this.incoming[wi])
+                            return;
+
                         // Message-passing data receipt
-                        let writeHead = this.incomingRW[1];
+                        const sbuf = this.incoming[wi];
+                        let writeHead = sbuf.heads[1];
                         const buf = ev.data.d;
                         const len = buf[0].length;
                         if (writeHead + len > bufSz) {
                             // We loop around
                             const brk = bufSz - writeHead;
                             for (let i = 0; i < this.incoming.length; i++) {
-                                this.incoming[i].set(buf[i%buf.length].subarray(0, brk), writeHead);
-                                this.incoming[i].set(buf[i%buf.length].subarray(brk), 0);
+                                sbuf.buffers[i].set(buf[i%buf.length].subarray(0, brk), writeHead);
+                                sbuf.buffers[i].set(buf[i%buf.length].subarray(brk), 0);
                             }
                         } else {
                             // Simple case
                             for (let i = 0; i < this.incoming.length; i++)
-                                this.incoming[i].set(buf[i%buf.length], writeHead);
+                                sbuf.buffers[i].set(buf[i%buf.length], writeHead);
                         }
                         writeHead = (writeHead + len) % bufSz;
-                        this.incomingRW[1] = writeHead;
+                        sbuf.heads[1] = writeHead;
                     };
+
                     break;
+                }
             }
         };
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     process(inputs: Float32Array[][], outputs: Float32Array[][], parameters: Record<string, Float32Array>) {
-        if (!this.workerPort || inputs.length === 0)
+        if (!this.workerPorts.length || inputs.length === 0)
             return true;
 
         // Find an input to use
@@ -108,127 +135,141 @@ class WorkerProcessor extends AudioWorkletProcessor {
             return true;
         const inp = inputs[inputIndex];
 
-        // SETUP
 
-        if (!this.incoming) {
-            const chans = inp.length;
-            this.incoming = [];
-            for (let i = 0; i < chans; i++) {
-                this.incoming.push(new Float32Array(
-                    this.canShared ?
-                        new SharedArrayBuffer(bufSz * 4)
-                      : new ArrayBuffer(bufSz * 4)
-                ));
-            }
-            this.incomingRW = new Int32Array(
-                this.canShared ?
-                    new SharedArrayBuffer(8)
-                  : new ArrayBuffer(8)
-            );
+        // For each worker we're connected to
+        for (let wi = 0; wi < this.workerPorts.length; wi++) {
+            const port = this.workerPorts[wi];
+            if (!port) continue;
+            let incoming = this.incoming[wi];
+            let outgoing = this.outgoing[wi];
 
-            if (this.canShared) {
-                // Don't need outgoing at all if we can't use shared memory
-                this.outgoing = [];
+            // SETUP
+
+            if (!incoming) {
+                const chans = inp.length;
+                this.incoming[wi] = incoming = {
+                    buffers: [],
+                    heads: new Int32Array(
+                        this.canShared ?
+                            new SharedArrayBuffer(8)
+                          : new ArrayBuffer(8)
+                        )
+                };
                 for (let i = 0; i < chans; i++) {
-                    this.outgoing.push(new Float32Array(new SharedArrayBuffer(bufSz * 4)));
+                    incoming.buffers.push(new Float32Array(
+                        this.canShared ?
+                            new SharedArrayBuffer(bufSz * 4)
+                          : new ArrayBuffer(bufSz * 4)
+                    ));
                 }
-                this.outgoingRW = new Int32Array(new SharedArrayBuffer(8));
 
-                // Tell the worker about our buffers
-                console.log("[INFO] AWP: Using shared memory");
-                this.workerPort.postMessage({
-                    c: "buffers",
-                    incoming: this.incoming,
-                    incomingRW: this.incomingRW,
-                    outgoing: this.outgoing,
-                    outgoingRW: this.outgoingRW
-                });
-            } else {
-                console.log("[INFO] AWP: Not using shared memory");
+                if (this.canShared) {
+                    // Don't need outgoing at all if we can't use shared memory
+                    this.outgoing[wi] = outgoing = {
+                        buffers: [],
+                        heads: new Int32Array(new SharedArrayBuffer(8))
+                    };
+                    for (let i = 0; i < chans; i++) {
+                        outgoing.buffers.push(new Float32Array(new SharedArrayBuffer(bufSz * 4)));
+                    }
+
+                    // Tell the worker about our buffers
+                    console.log("[INFO] AWP: Using shared memory");
+                    port.postMessage({
+                        c: "buffers",
+                        incoming: incoming.buffers,
+                        incomingRW: incoming.heads,
+                        outgoing: outgoing.buffers,
+                        outgoingRW: outgoing.heads
+                    });
+                } else {
+                    console.log("[INFO] AWP: Not using shared memory");
+                }
             }
-        }
 
-        // INPUT (outgoing)
+            // INPUT (outgoing)
 
-        // Transmit our current data
-        if (this.canShared) {
-            // Write it into the buffer
-            let writeHead = this.outgoingRW[1];
-            const len = inp[0].length;
-            if (writeHead + len > bufSz) {
+            // Transmit our current data
+            if (this.canShared) {
+                // Write it into the buffer
+                let writeHead = outgoing.heads[1];
+                const len = inp[0].length;
+                if (writeHead + len > bufSz) {
+                    // We wrap around
+                    const brk = bufSz - writeHead;
+                    for (let i = 0; i < this.outgoing.length; i++) {
+                        outgoing.buffers[i].set(inp[i%inp.length].subarray(0, brk), writeHead);
+                        outgoing.buffers[i].set(inp[i%inp.length].subarray(brk), 0);
+                    }
+                } else {
+                    // Simple case
+                    for (let i = 0; i < this.outgoing.length; i++)
+                        outgoing.buffers[i].set(inp[i%inp.length], writeHead);
+                }
+                writeHead = (writeHead + len) % bufSz;
+                Atomics.store(outgoing.heads, 1, writeHead);
+
+                // Notify the worker
+                Atomics.notify(outgoing.heads, 1);
+
+            } else {
+                /* Just send the data, along with a timestamp. Minimize allocation
+                 * by sending plain */
+                port.postMessage(Date.now());
+                port.postMessage(inp);
+
+            }
+
+            // OUTPUT (incoming)
+
+            let readHead: number = incoming.heads[0];
+            let writeHead: number;
+            if (this.canShared)
+                writeHead = Atomics.load(incoming.heads, 1);
+            else
+                writeHead = incoming.heads[1];
+            if (readHead === writeHead)
+                continue;
+            let len = writeHead - readHead;
+            if (len < 0)
+                len += bufSz;
+
+            // Drain any excess buffer
+            if (len > 4800) {
+                readHead = writeHead - 4800;
+                if (readHead < 0)
+                    readHead += bufSz;
+            }
+
+            // Don't use too little data
+            if (len < outputs[0].length)
+                continue;
+
+            // Finally, send the buffered output
+            const out = outputs[0];
+            const readEnd = (readHead + out[0].length) % bufSz;
+            if (readEnd < readHead) {
                 // We wrap around
-                const brk = bufSz - writeHead;
-                for (let i = 0; i < this.outgoing.length; i++) {
-                    this.outgoing[i].set(inp[i%inp.length].subarray(0, brk), writeHead);
-                    this.outgoing[i].set(inp[i%inp.length].subarray(brk), 0);
+                const brk = bufSz - readHead;
+                for (let i = 0; i < out.length; i++) {
+                    out[i].set(incoming.buffers[i%incoming.buffers.length].subarray(readHead), 0);
+                    out[i].set(incoming.buffers[i%incoming.buffers.length].subarray(0, readEnd), brk);
                 }
             } else {
                 // Simple case
-                for (let i = 0; i < this.outgoing.length; i++)
-                    this.outgoing[i].set(inp[i%inp.length], writeHead);
+                for (let i = 0; i < out.length; i++) {
+                    out[i].set(incoming.buffers[i%incoming.buffers.length].subarray(readHead, readEnd), 0);
+                }
             }
-            writeHead = (writeHead + len) % bufSz;
-            Atomics.store(this.outgoingRW, 1, writeHead);
 
-            // Notify the worker
-            Atomics.notify(this.outgoingRW, 1);
-
-        } else {
-            /* Just send the data, along with a timestamp. Minimize allocation
-             * by sending plain */
-            this.workerPort.postMessage(Date.now());
-            this.workerPort.postMessage(inp);
-
-        }
-
-        // OUTPUT (incoming)
-
-        let readHead: number = this.incomingRW[0];
-        let writeHead: number;
-        if (this.canShared)
-            writeHead = Atomics.load(this.incomingRW, 1);
-        else
-            writeHead = this.incomingRW[1];
-        if (readHead === writeHead)
-            return true;
-        let len = writeHead - readHead;
-        if (len < 0)
-            len += bufSz;
-
-        // Drain any excess buffer
-        if (len > 4800) {
-            readHead = writeHead - 4800;
-            if (readHead < 0)
-                readHead += bufSz;
-        }
-
-        // Don't use too little data
-        if (len < outputs[0].length)
-            return true;
-
-        // Finally, send the buffered output
-        const out = outputs[0];
-        const readEnd = (readHead + out[0].length) % bufSz;
-        if (readEnd < readHead) {
-            // We wrap around
-            const brk = bufSz - readHead;
-            for (let i = 0; i < out.length; i++) {
-                out[i].set(this.incoming[i%this.incoming.length].subarray(readHead), 0);
-                out[i].set(this.incoming[i%this.incoming.length].subarray(0, readEnd), brk);
+            // And update the read head
+            if (this.canShared) {
+                Atomics.store(incoming.heads, 0, readEnd);
+                Atomics.notify(incoming.heads, 0);
+            } else {
+                incoming.heads[0] = readEnd;
             }
-        } else {
-            // Simple case
-            for (let i = 0; i < out.length; i++) {
-                out[i].set(this.incoming[i%this.incoming.length].subarray(readHead, readEnd), 0);
-            }
-        }
 
-        // And update the read head
-        if (this.canShared) {
-            Atomics.store(this.incomingRW, 0, readEnd);
-            Atomics.notify(this.incomingRW, 0);
-        } else {
-            this.incomingRW[0] = readEnd;
         }
 
         return true;
