f69fec8d22a79e3e3120f3791ee7f62332edc84a
Yahweasel <git-rpo@yahweasel.com>
2022-08-25 17:13:30 -0400

    Need the anull filter in libav.js

--- libav/mkconfig.sh
+++ libav/mkconfig.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-exec ./mkconfig.js ennuicastr '["ogg","webm","opus","ipod","flac","flt","h264","audio-filters"]'
+set -ex
+test -d fragments
+mkdir fragments/ennuicastr
+echo '--enable-filter=anull' > fragments/ennuicastr/ffmpeg-config.txt
+./mkconfig.js ennuicastr '["ogg","webm","opus","ipod","flac","flt","h264","ennuicastr","audio-filters"]'
+rm -r fragments/ennuicastr
