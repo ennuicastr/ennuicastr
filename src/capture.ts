4e67fdd0b812893673a126e9714e0631c007d08b
Yahweasel <git-rpo@yahweasel.com>
2022-12-03 14:28:10 +0000

    Version bumps

--- src/capture.ts
+++ src/capture.ts
@@ -27,7 +27,7 @@ import * as util from "./util";
 import * as rtennui from "rtennui";
 
 // Worker path to use
-const workerVer = "w." + Math.random() + Math.random() + Math.random();
+const workerVer = "x";
 export const workerPath = "awp/ennuicastr-worker.js?v=" + workerVer;
 
 export interface Capture {
