43e2e9e0c6fe40eabf7470abe56eda80a7197207
Yahweasel <git-rpo@yahweasel.com>
2023-12-04 18:38:37 +0000

    Updating dependencies

--- libav/mkconfig.sh
+++ libav/mkconfig.sh
@@ -4,7 +4,8 @@ test -d fragments
 ./mkconfig.js ennuicastr '[
     "format-ogg","format-webm", "format-mp4",
     "codec-libopus", "codec-flac","codec-pcm_f32le",
-    "decoder-h264",
-    "bsf-vp9_metadata",
+    "parser-vp8", "libvpx", "decoder-libvpx_vp8",
+    "parser-h264", "bsf-h264_metadata",
+    "parser-vp9", "bsf-vp9_metadata",
     "audio-filters","swscale"
 ]'
--- src/capture.ts
+++ src/capture.ts
@@ -27,7 +27,7 @@ import * as util from "./util";
 import * as rtennui from "rtennui";
 
 // Worker path to use
-const workerVer = "12";
+const workerVer = "13";
 export const workerPath = "awp/ennuicastr-worker.js?v=" + workerVer;
 
 export interface Capture {
